995d9b073ef1c93e1d8bf0e8ac55951836f66833
mfaizk <faiz-react@tarality.com>
2025-08-08 15:23:01 +0530

    minor ui fix

--- app/layout.js
+++ app/layout.js
@@ -30,47 +30,14 @@ export const metadata = {
     "binance",
   ],
   icons: {
-    icon: ["/assets/brand/onlyLogo.png"],
-    apple: ["/assets/brand/onlyLogo.png"],
-    shortcut: ["/assets/brand/onlyLogo.png"],
+    icon: "/assets/brand/onlyLogo.png",
+    apple: "/assets/brand/onlyLogo.png",
+    shortcut: "/assets/brand/onlyLogo.png",
   },
   other: {
     rel: "mask-icon",
     url: "/assets/brand/onlyLogo.png",
   },
-  //////////////////asasd///////////
-
-  // openGraph: {
-  //   title:
-  //     "NOWA – AI-Powered Crypto Price Prediction Platform | Predict & Earn with $NOW Token",
-  //   description:
-  //     "Join NOWA, the leading AI-powered crypto prediction app on Binance Smart Chain. Beat AI forecasts, stake $NOW tokens, and earn crypto rewards. Participate in global competitions today!",
-  //   url: `${base_url}`,
-  //   siteName: "NOWA",
-  //   images: [
-  //     {
-  //       url: `${base_url}/assets/brand/onlyLogo.png`,
-  //       width: 1200,
-  //       height: 630,
-  //       alt: "NOWA",
-  //       type: "image",
-  //     },
-  //   ],
-  //   type: "website",
-  //   locale: "en_US",
-  // },
-
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title:
-  //     "NOWA – AI-Powered Crypto Price Prediction Platform | Predict & Earn with $NOW Token",
-  //   description:
-  //     "Join NOWA, the leading AI-powered crypto prediction app on Binance Smart Chain. Beat AI forecasts, stake $NOW tokens, and earn crypto rewards. Participate in global competitions today!",
-  //   images: [`${base_url}/assets/brand/onlyLogo.png`],
-  // },
-
-  // authors: [{ name: "NOWA Team", url: `${base_url}` }],
-  // creator: "NOWA Development",
 
   robots: {
     index: true,
